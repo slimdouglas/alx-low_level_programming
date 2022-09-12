a0faf58983691dcde5699c00c3e23a6e58a79adc
slimdouglas <nuerjimm34@gmail.com>
2022-09-11 17:04:27 -0700

    c03

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - initializer, single line
+ * @: Description of parameter x
+(* a blank line
+ * Description: This function prints a to z in
+ * lower case)?
+(* section header: Section description)*
+ * Return: Terminates the code
+ */
+int main()
+{
+	char ny = 'a';
+
+	while (ny <= 'z')
+	{
+		putchar(ny);
+		putchar('\n');
+		ny++;
+	}
+	return 0;
+}
