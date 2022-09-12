9ef4d566ad06463070c5227cf2fab69a208b0131
slimdouglas <nuerjimm34@gmail.com>
2022-09-12 12:29:57 -0700

    co55

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: returns zero value
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
