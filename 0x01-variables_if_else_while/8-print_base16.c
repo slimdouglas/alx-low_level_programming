276cc4e1cd1368d59be9637d413411b7d5fb2c4b
slimdouglas <nuerjimm34@gmail.com>
2022-09-12 10:52:53 -0700

    c056

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+/**
+ * main - Short description, single line
+ * @void : Description of parameter x
+(* a blank line
+ * Description: Prints all base 16 digits)?
+(* section header: Section description)*
+ * Return: Returns zero
+ */
+int main(void)
+{
+	unsigned char m;
+
+	for (m = 0; m <= 'F'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
