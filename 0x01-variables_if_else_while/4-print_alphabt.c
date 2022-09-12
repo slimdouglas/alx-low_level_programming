1ed99a82ad72eb94370e03b3d4306776242a55a3
slimdouglas <nuerjimm34@gmail.com>
2022-09-12 08:24:05 -0700

    c06

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Returns no value
+ */
+
+
+int main(void)
+{
+	char ny = 'a';
+
+	while (ny <= 'z' && ny != 'q' && ny != 'e')
+	{
+		putchar(ny);
+		ny++;
+	}
+	putchar('\n');
+	return (0);
+}
