364e0a85ef771198329cf6a163c7556740c1990c
slimdouglas <nuerjimm34@gmail.com>
2022-09-12 09:42:23 -0700

    xod

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void: Means no output x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
