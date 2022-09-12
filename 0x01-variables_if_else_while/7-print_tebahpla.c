73a59e7e16b624d90f680717e78ade05709977f1
slimdouglas <nuerjimm34@gmail.com>
2022-09-12 10:06:48 -0700

    c099

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Prints z-a )?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
