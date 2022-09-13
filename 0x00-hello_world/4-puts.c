0096f78d90f74f6c4da4d6872a2ad6373ded73a8
slimdouglas <nuerjimm34@gmail.com>
2022-09-12 17:50:47 -0700

    cv

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+/**
+ * main- Short description, single line
+ * @void : Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	char mystr[] = "Programming is like building a multilingual puzzle";
+
+		puts(mystr);
+	return (0);
+}
+
