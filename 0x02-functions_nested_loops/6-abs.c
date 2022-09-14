67f404fe4c1947f7c990e8e6e323d856d79bcc60
slimdouglas <nuerjimm34@gmail.com>
2022-09-14 13:13:02 -0700

    c012

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _abs - returns absolute value of a ch
+ * @n: int n
+(*
+ * Description: abs value of input)?
+(* section header: Section description)*
+ * Return: returns absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+}
