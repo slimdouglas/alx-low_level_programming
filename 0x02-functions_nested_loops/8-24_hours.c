8098092be34d7b4fe36ade6d84caa482ab0b88fc
slimdouglas <nuerjimm34@gmail.com>
2022-09-14 14:09:34 -0700

    nl09

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints evry hour and minute
+(*
+ * Description: Returns values of hrs and min)?
+(* section header: Section description)*
+ * Return: void no return value
+ */
+void jack_bauer(void)
+{
+	for (hrs = 0; hrs <= 23; ++hrs)
+	{
+		for (mn =0; mn <= 59; ++mn)
+		{
+			_putchar((hrs / 10) + 48);
+			_putchar((hrs % 10) + 48);
+			_putchar(':');
+			_putchar((mn / 10) + 48);
+			_putchar((mn % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
