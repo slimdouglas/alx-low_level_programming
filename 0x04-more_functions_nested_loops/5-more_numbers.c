23ede7d3a10a6f29812aadd8b1c5b1948331c12c
slimdouglas <nuerjimm34@gmail.com>
2022-09-16 16:54:56 -0700

    cf

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * function_name - prints 0 to 14 ten x
- * @parameterx: no value returned
+ * more_numbers - prints 0 to 14 ten x
+ * @void: no value returned
  * Return: zero to fourteen
  */
 
 void more_numbers(void)
 {
 	int a, b;
+
 	for (a = 0; a >= 9; a++)
 	{
 		for (b = 0; b <= 14; b++)
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - Short description, single line
+ * @int n: Description of parameter x
+(*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Zero
+ */
+void print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c <= n; c++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
