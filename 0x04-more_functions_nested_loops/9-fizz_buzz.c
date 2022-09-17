47104c6f503ad5c3bb5ec13a48afa23c96aea2ea
slimdouglas <nuerjimm34@gmail.com>
2022-09-16 20:08:14 -0700

    gg

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,11 @@ int main(void)
 	for (i = 1; i <=100 ; i++)
 	{
 		if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
-		if else (i % 5 == 0 )
-		{
+		else if (i % 5 == 0 )
 			printf("Buzz");
-		}
-		if else (i % 15 == 0)
-		{
+		else if (i % 15 == 0)
 			printf("FizzBuzz");
-		}
 		else
 		{
 			printf("%d ", i);
