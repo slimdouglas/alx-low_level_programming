5cea7e10fbd14803660faca92b038c6fb89be0c6
slimdouglas <nuerjimm34@gmail.com>
2022-09-20 17:58:24 -0700

    pointer6

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
+/**
+ * _strlen - returns string count
+ * @s: character s
+ *
+ * Return: lengths of string
+ */
+
 int _strlen(char *s)
 {
-	strlen(s);
+	int count;
+
+	for (count = 0; s[count] != '\0'; ++count);
+
+	return count;
 }
