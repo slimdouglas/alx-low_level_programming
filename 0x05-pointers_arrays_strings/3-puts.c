ac944f97fb0cab817130a9c62bb12123fd72473b
slimdouglas <nuerjimm34@gmail.com>
2022-09-20 18:25:08 -0700

    gistring

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - returns the string
+ * @str: input string
+ *
+ * Return: returns the whole string
+ */
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+	return (0);
+}
