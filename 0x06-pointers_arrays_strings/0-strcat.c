104791f13d2d5281b314b46d848bf22d4ee365dc
slimdouglas <nuerjimm34@gmail.com>
2022-09-22 09:47:04 -0700

    concsat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strcat - concats two strings
+ * @dest: string 1
+ * @src: string two
+ * Return: zero
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char str3[100];
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		str3[j] = dest[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		str3[j] = src[i];
+		i++;
+		j++;
+	}
+	str3[j] = '\0';
+	return (0);
+}
