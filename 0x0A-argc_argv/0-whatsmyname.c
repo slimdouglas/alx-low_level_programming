f2135b295045b78ba72a17acfe268c0952f3e29e
slimdouglas <nuerjimm34@gmail.com>
2022-09-30 08:19:18 -0700

    arg1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - intitializer
+ * @argc: counts number of variables
+ * @argv: pointer to array of strings
+ * Return: zero if success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
