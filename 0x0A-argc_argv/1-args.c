aad1461663b454d3138457f9054a86b99388fdf1
slimdouglas <nuerjimm34@gmail.com>
2022-09-30 09:16:22 -0700

    arg5

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - intitializer
+ * @argc: count of arguments
+ * Return: zero
+ */
+int _1main(int argc)
+{
+
+	printf("%d\n", argc);
+	return (0);
+}
