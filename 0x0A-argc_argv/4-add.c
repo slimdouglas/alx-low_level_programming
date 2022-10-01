f077ac8e8559c04442857c2aa96786af4152b6fd
slimdouglas <nuerjimm34@gmail.com>
2022-10-01 13:38:59 -0700

    arg19

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,39 @@
 #include "main.h"
 #include<stdio.h>
 #include<stdlib.h>
+#include <ctype.h>
 
 /**
  * main - initializer
- * @argc: counter 
- * @ argv: array of arguments
+ * @argc: counter
+ * @argv: array of arguments
  * Return: zero`
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, p, sum = 0;
+
 	if (argc > 1)
 	{
 		for (i = 1; i < argc ; i++)
 		{
+			p = atoi(argv[i]);
+			if (!isdigit(p))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
 			/*argv_2 = strtol(argv[i], NULL, 10);*/
-			sum += atoi(argv[i]);
+				printf("Error\n");
+				return (1);
+			}
 		}
 		printf("%d\n", sum);
 	}
 	else
 	{
-		return (0);
+		printf("0\n");
 	}
 	return (0);
 }
