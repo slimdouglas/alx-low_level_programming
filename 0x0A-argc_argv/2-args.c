65b9419c1154d679aefff615ec8a3132944fe4f6
slimdouglas <nuerjimm34@gmail.com>
2022-10-01 09:13:01 -0700

    arg17

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,10 @@
  * @argv: pointer to array of args
  * Return: returns zero for success
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
