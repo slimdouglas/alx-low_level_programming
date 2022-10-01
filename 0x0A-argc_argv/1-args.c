d5705fc7294487926fed4ef6700c27f85d9f7ed3
slimdouglas <nuerjimm34@gmail.com>
2022-10-01 09:03:25 -0700

    arg15

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,15 +4,17 @@
 /**
  * main - intitializer
  * @argc: count of arguments
+ * @argv: pointer to arguments
  * Return: zero
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	if (argv[i] => 0)
-	{
 	printf("%d\n", argc);
+	if (argc > 100)
+	{
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
