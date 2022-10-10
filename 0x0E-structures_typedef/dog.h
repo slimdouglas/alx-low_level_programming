7fc4e0375346a291ff7f0d6c7cfb534398577128
slimdouglas <nuerjimm34@gmail.com>
2022-10-10 13:57:33 -0700

    struct1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * dog - strcut dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Description: struct for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
