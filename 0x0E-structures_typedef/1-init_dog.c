f286de5a66bcbad1ec8ae4068814dcb84a72db42
slimdouglas <nuerjimm34@gmail.com>
2022-10-10 15:31:17 -0700

    struct3

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * init_dog - initializes the struct dog
+ * struct dog *d - strcuture dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owner of the dog
+ *
+ * Description: short description of dog struct
+ * Return: zero if success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d
+	{
+		name;
+		age;
+		owner;
+	};
+	return (0);
+}
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 struct dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* MAIN_H */
